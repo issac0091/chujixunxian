4de2a0203c02d835f021d91101a5cbe6e34c9280
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-06 11:36:40 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,18 @@
+cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
+basic.showIcon(IconNames.Happy)
+if (!(input.buttonIsPressed(Button.A))) {
+    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
+}
+basic.forever(function () {
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 150)
+    } else if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 200)
+    } else if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 200)
+    } else {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 150)
+        basic.pause(80)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
+    }
+})
