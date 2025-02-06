83338cc269313ba49556243b4c4b16b1af90d14c
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-06 11:41:44 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,11 +8,11 @@ basic.forever(function () {
         cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 150)
     } else if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
         cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 200)
-    } else if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 200)
+    } else if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 220)
     } else {
         cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 150)
         basic.pause(80)
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 100)
     }
 })
