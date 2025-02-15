60f8d4905d37983aa17e6a2f442c746f6310ae44
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-15 15:09:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ if (!(input.buttonIsPressed(Button.A))) {
     basic.showString("home")
 }
 basic.forever(function () {
-    basic.showNumber(input.acceleration(Dimension.Strength))
+	
 })
 basic.forever(function () {
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
